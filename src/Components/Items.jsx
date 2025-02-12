b6e771724f2ade4aa88c4a47e467f5e6f8b0f6f4
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-02-12 20:03:31 +0530

    update code

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -1,17 +1,19 @@
+import image from '../assets/default.jpg' 
 
 
-const Items = () => {
-  return (
-    
-    <div className="card bg-dark text-light mb-3 d-inline-block m-4 p-4" style={{maxWidth:"350px"}}>
-  <img src="" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">title</h5>
-    <p className="card-text">News is not available now, we can fix the problem soon,  we will be back soon. </p>
-    <a href="/" className="btn btn-primary">Read more</a>
+
+const Items ({title,description,src,url}) => {
+    return (
+      
+      <div className="card bg-dark text-light mb-3 d-inline-block m-4 p-4" style={{maxWidth:"350px"}}>
+    <img src={src?src:image} style={{height:"200px",width:"300px"}} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">{title.slice(0,50)}</h5>
+      <p className="card-text">{description?description.slice(0,90):"News is not available now, we can fix the problem soon,  we will be back soon. "}</p>
+      <a href={url} className="btn btn-primary">Read more</a>
+    </div>
   </div>
-</div>
-  )
-}
+    )
+  }
 
 export default Items
