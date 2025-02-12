a1572173bf7d4eb3468fdac8bd95b3807ee3e838
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-02-12 20:00:10 +0530

    Item Page

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -2,9 +2,15 @@
 
 const Items = () => {
   return (
-    <div>
-        
-    </div>
+    
+    <div className="card bg-dark text-light mb-3 d-inline-block m-4 p-4" style={{maxWidth:"350px"}}>
+  <img src="" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">title</h5>
+    <p className="card-text">News is not available now, we can fix the problem soon,  we will be back soon. </p>
+    <a href="/" className="btn btn-primary">Read more</a>
+  </div>
+</div>
   )
 }
 
