34a28aa1546765f7e030df7565d6b401e66d2b47
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-02-13 14:15:14 +0530

    Fix Ui

--- src/Components/Navabar.jsx
+++ src/Components/Navabar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({setCategory}) => {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
+    <div className="collapse navbar-collapse" id="Nabar">
       <ul className="navbar-nav">
         
         <li className="nav-item">
